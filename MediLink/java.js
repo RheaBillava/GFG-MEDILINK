6796e9dd234c67292ef226d436b6362a319c91a2
RheaBillava <97047444+RheaBillava@users.noreply.github.com>
2023-04-15 23:08:27 +0530

    Add files via upload

---
+++ MediLink/java.js
@@ -0,0 +1,10 @@
+// document.querySelector("body > main > header")
+const successCallback = (position) => {
+    console.log(position);
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
